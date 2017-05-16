b1b71ce9bb66f45005bf0c1a752850e3f5691097
rooley <rooley@mail.ru>
2017-05-16 17:08:11 +0300

    gradle project

---
+++ src/main/java/org/school/FirstprojectApplication.java
@@ -0,0 +1,13 @@
+package org.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstprojectApplication.class, args);
+//		System.out.println("first project works");
+	}
+}
