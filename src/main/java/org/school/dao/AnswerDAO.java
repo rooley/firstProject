88472638e07af7d00f48700449fa95bd4833d7c2
user <user@21-000849>
2017-05-18 13:20:52 +0300

    modify interface

--- src/main/java/org/school/dao/AnswerDAO.java
+++ src/main/java/org/school/dao/AnswerDAO.java
@@ -1,15 +1,21 @@
 package org.school.dao;
 
+import java.util.List;
+
 import org.school.domain.Answer;
 
 public interface AnswerDAO {
-	
-	public void insert(Answer answer);
-	
-	public Answer findById(long id);
-	
-	public void setRate(long id, double rate);
-	
-	public double getSumRate();
-	
+
+	public Answer findByIdAnswer(long id);
+
+	public Answer findByIdQuestionInAnswer(long questionId);
+
+	public List<Answer> getAllAnswer();
+
+	public void insertAnswer(Answer answer);
+
+	public void setRate(double rate);
+
+	// public double getSumRate();
+
 }
